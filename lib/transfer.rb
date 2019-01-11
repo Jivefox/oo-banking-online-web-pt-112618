573983ae9e6beba222de4956a2fc07fce5391dd4
Cliff Jurgensen <jivefox.002@gmail.com>
2019-01-11 07:47:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,8 @@ class Transfer
  
  def execute_transaction
    if self.valid? && sender.balance > amount && self.status == "pending"
-     sender.balance += amount
-     receiver.balance -= amount
+     receiver.balance += amount
+     sender.balance -= amount
      self.status = "complete"
    end
  end
